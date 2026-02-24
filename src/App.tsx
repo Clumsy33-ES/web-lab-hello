0a2c1aa29b847f456edddcdec32b45f6e75911f1
Clumsy33-ES <alperenelif3@gmail.com>
2026-02-24 17:22:24 +0300

    chore: initial project setup with Vite + React +TS

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+  
+
+return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: Elif SAKAR</p>
+<p>Ogrenci No: 230541088</p>
+</div>
+)
+}
+
+export default App
